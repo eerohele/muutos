f97fcf1c843381b8510ce27b4e55ffce63e61a36
Eero Helenius <31859+eerohele@users.noreply.github.com>
2026-02-17 09:52:33 +0200

    Refactor concurrently to use AutoCloseable/.close

--- test/muutos/test/concurrency.clj
+++ test/muutos/test/concurrency.clj
@@ -1,22 +1,16 @@
 (ns muutos.test.concurrency
-  (:import (java.util.concurrent CountDownLatch Executors)
-           (java.util.concurrent TimeUnit)))
+  (:import (java.util.concurrent CountDownLatch Executors)))
 
 (set! *warn-on-reflection* true)
 
 (defmacro concurrently
   [{:keys [threads]
     :or {threads 1000}} & body]
-  `(let [executor# (Executors/newVirtualThreadPerTaskExecutor)]
-     (try
-       (let [latch# (CountDownLatch. ~threads)]
-         (dotimes [_i# ~threads]
-           (.execute executor#
-             (^:once fn* []
-              (.countDown latch#)
-              (.await latch#)
-              (do ~@body)))))
-       (finally
-         (.shutdown executor#)
-         (.awaitTermination executor#
-           Long/MAX_VALUE TimeUnit/MILLISECONDS)))))
+  `(with-open [executor# (Executors/newVirtualThreadPerTaskExecutor)]
+     (let [latch# (CountDownLatch. ~threads)]
+       (dotimes [_i# ~threads]
+         (.execute executor#
+           (^:once fn* []
+            (.countDown latch#)
+            (.await latch#)
+            (do ~@body)))))))
