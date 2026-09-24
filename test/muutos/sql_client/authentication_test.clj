b10eab94ad6a64c82b0acc844ad271031931a79f
Eero Helenius <31859+eerohele@users.noreply.github.com>
2026-09-24 20:29:15 +0300

    Add assertion

--- test/muutos/sql_client/authentication_test.clj
+++ test/muutos/sql_client/authentication_test.clj
@@ -6,7 +6,12 @@
 
 (deftest ^:integration scram-sha-256
   (with-open [pg (connect)]
-    (is (= [{"a" 1}] (sq pg "SELECT 1 AS a")))))
+    (is (= [{"a" 1}] (sq pg "SELECT 1 AS a")))
+    (is (= [{"ssl" true
+             "cipher" "TLS_AES_256_GCM_SHA384"
+             "version" "TLSv1.3"
+             "bits" 256}]
+          (sq pg "SELECT ssl, cipher, version, bits FROM pg_stat_ssl WHERE pid = pg_backend_pid()")))))
 
 (deftest ^:integration trust
   (with-open [pg (connect :port 5437)]
